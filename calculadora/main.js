9113309c91261b445c9c5d5cbf7a1df24bec8fe3
Markucho18 <marcossosa21415@gmail.com>
2025-07-02 21:55:53 -0300

    Calculadora: Implemente historial(modificar y borrar), mostrarPantalla solo concatena el array

--- calculadora/main.js
+++ calculadora/main.js
@@ -4,15 +4,42 @@ const pantalla = document.getElementById("pantalla")
 
 let valorPantalla = ""
 
+let historial = []
+
 let resultadoActual = 0
 
-const mostrarEnPantalla = (valor) => {
-    valorPantalla += ` ${valor}`
-    pantalla.textContent = valorPantalla
+const modificarHistorial = (valor) => {
+    const ultimoElemento = historial[ historial.length - 1]
+    if(parseInt(valor) && parseInt(ultimoElemento)){
+        historial[historial.length - 1] = ultimoElemento + valor
+    } else{
+        historial.push(valor)
+    }
+    console.log(historial)
+    //Tambien debo comprobar si el ultimo elemento es una operacion no añadirlo, por que no hay dos opereaciones seguidas
 }
 
 const borrar = () => {
-    valorPantalla = valorPantalla.slice(0, valorPantalla.length - 2)
+    const ultimoElemento = historial[ historial.length - 1]
+    if(ultimoElemento.length > 1){
+        historial[historial.length - 1] = ultimoElemento.slice(0, -1)
+    } else{
+        historial.pop()
+    }
+    mostrarEnPantalla()
+}
+
+const calcularResultado = () => {
+    console.log("calculando resultado...")
+    //Hay que simular la prioridad 
+    let total = 0
+    historial.forEach((elemento, i) => {
+        
+    })
+}
+
+const mostrarEnPantalla = (valor) => {
+    valorPantalla = historial.join(" ")
     pantalla.textContent = valorPantalla
 }
 
@@ -24,10 +51,17 @@ for(let i = 0; i < botones.length; i++){
     if(boton.id === "botonBorrar"){
         boton.addEventListener("click", borrar)
     }
+    else if(boton.id === "botonResultado"){
+        boton.addEventListener("click", calcularResultado)
+    }
     else {
-        boton.addEventListener("click", () => mostrarEnPantalla(valor))
+        boton.addEventListener("click", () =>{
+            modificarHistorial(valor)
+            mostrarEnPantalla()
+        })
     }
 }
+
 
 //Casos que se deben contemplar:
 //La division y la multiplicacion se hacen primero, lo cual dificulta hacer el resultado sobre la marcha
